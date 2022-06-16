9761acba1932e5bf9e9a4b931a4ef30448cf2e42
Rutvikk :D <rutvikk5@gmail.com>
2022-06-16 17:55:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -16,3 +16,17 @@ async function generatePickUpLine() {
   const data = await response.json()
   console.log(data)
 }
+
+/*const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'cff20a77d0msh899191bba8e5c6ap101fcajsnc53377c0c29b',
+		'X-RapidAPI-Host': 'twitter-scraper2.p.rapidapi.com'
+	}
+};
+
+fetch('https://twitter-scraper2.p.rapidapi.com/api/v2/search?allOfTheseWords=Hey%20girl%20Cuz%20&lang=en&fromTheseAccounts=%40Rutvikk05&fromDate=2022-01-01&searchMode=live', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+*/
