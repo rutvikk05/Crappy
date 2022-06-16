00f5f8eddd549ce56ba9aa5ab73df45dd1a0fc49
Rutvikk :D <rutvikk5@gmail.com>
2022-06-16 18:02:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const pickupLine = document.getElementById('PLine')
 const pickupLineButton = document.getElementById('pLineBtn')
 pickupLineButton.addEventListener('click', generatePickUpLine)
 const url =
-  'https://twitter-scraper2.p.rapidapi.com/api/v2/search?allOfTheseWords=Hey%20girl%20Cuz&lang=en&fromTheseAccounts=%40rutvikk05&fromDate=2022-01-01&searchMode=live'
+ 'https://twitter-scraper2.p.rapidapi.com/api/v2/search?allOfTheseWords=Hey%20girl%20Cuz%20&lang=en&fromTheseAccounts=%40Rutvikk05&fromDate=2022-01-01&searchMode=live'
 const options = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': '9471d7bc0fmsh7dbb39fb9f8f612p1029eejsn01706ecf6dff',
+    'X-RapidAPI-Key': 'cff20a77d0msh899191bba8e5c6ap101fcajsnc53377c0c29b',
     'X-RapidAPI-Host': 'twitter-scraper2.p.rapidapi.com',
   },
 }
@@ -16,17 +16,3 @@ async function generatePickUpLine() {
   const data = await response.json()
   console.log(data)
 }
-
-/*const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'cff20a77d0msh899191bba8e5c6ap101fcajsnc53377c0c29b',
-		'X-RapidAPI-Host': 'twitter-scraper2.p.rapidapi.com'
-	}
-};
-
-fetch('https://twitter-scraper2.p.rapidapi.com/api/v2/search?allOfTheseWords=Hey%20girl%20Cuz%20&lang=en&fromTheseAccounts=%40Rutvikk05&fromDate=2022-01-01&searchMode=live', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
-*/
